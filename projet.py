0f54740716bbf607178637edd2d7527521a84ac9
balti samar <saamarhamdi123@gmail.com>
2025-03-11 12:46:12 -1100

    Fusion de projets1 dans main avec un seul commit

--- projet.py
+++ projet.py
@@ -7,7 +7,6 @@ Data=[1,3,5]
 Som=sum(Data)
 print ('la somme est:', Somme(data))
 
-
 Data = [1,3,5]
 if Data:
     print ('la somme est:', sum(Data))
@@ -20,5 +19,8 @@ else:
 
 
 
+
+=======
+>>>>>>> projets1
 =======
 >>>>>>> projets1
