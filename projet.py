ea63f6474cc251a5d2f1d772ad398e67d44a00fb
balti samar <saamarhamdi123@gmail.com>
2025-03-11 12:41:31 -1100

    Fusion de projets1 dans main avec un seul commit

--- projet.py
+++ projet.py
@@ -1,5 +1,14 @@
 
+
 Data=[2,4,6]
 Data=[1,3,5]
 Som=sum(Data)
 print ('la somme est:', Som)
+Data = [1,3,5]
+if Data:
+    print ('la somme est:', sum(Data))
+    print ('le min est:', min(Data))
+    print ('le max est:', max(Data))
+else:
+    print ('dossier vide')
+
